e25e4d60b5e0bcc01ecff80d934b1e49e8fd0640
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2022-10-06 23:52:40 -0300

    solucion avanzada

--- nuevo-funciones.js
+++ nuevo-funciones.js
@@ -2,6 +2,12 @@ let iterando = true
 
 const terminar = () => { iterando = false }
 
+const solicitarDatos = (id = productos.length + 1) => {
+    const nombre = prompt('Ingrese nombre');
+    const precio = prompt('Ingrese precio');
+    return { id, nombre, precio };
+}
+
 const getId = () => Number(prompt('ingrese el id'))
 
 const checkId = (id) => productos.some(producto => producto.id === id)
@@ -12,10 +18,6 @@ const getIndex = (id) => productos
             producto.id === id)
     )
 
-const solicitarDatos = (id = productos.length + 1) => {
-    const nombre = prompt('Ingrese nombre');
-    const precio = prompt('Ingrese precio');
-    return { id, nombre, precio };
-}
+
 
 
